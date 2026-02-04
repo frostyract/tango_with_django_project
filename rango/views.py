6fcb0cfcd84e50f7da549d9283056a95af5f3589
frostyract <frostyerroroof@protonmail.com>
2026-02-04 14:03:08 +0000

    added hyperlinks

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # views go here
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    return HttpResponse("Rango says here is the about page. <a href='/rango'>Index</a>")
