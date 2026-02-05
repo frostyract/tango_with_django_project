e333190f2f01cd8a3bffa4a1bbf6b7705e7826aa
frostyract <frostyerroroof@protonmail.com>
2026-02-05 16:18:42 +0000

    chapter 6 done with exercises

--- populate_rango.py
+++ populate_rango.py
@@ -10,33 +10,41 @@ def populate():
     # then create dictionary for our categories and put the page dictionary list into them
     python_pages = [
             {"title": "Official Python Tutorial",
-             "url":"http://docs.python.org/3/tutorial"
+             "url":"http://docs.python.org/3/tutorial",
+             "views":5
             },
             {"title": "How to Think like a Computer Scientist",
-             "url":"http://greenteapress.com/thinkpython/"
+             "url":"http://greenteapress.com/thinkpython/",
+             "views":50
              },
             {"title":"Learn Python in 10 Minutes",
-             "url":"httpL//www.korokithakis.net/tutorials/python/"},
+             "url":"http://www.korokithakis.net/tutorials/python/",
+             "views":1},
     ]
 
     django_pages = [
             {"title":"Official Django Tutorial",
-             "url":"https://docs.djangoproject.com/en/2.1/intro/tutorial01/"
+             "url":"https://docs.djangoproject.com/en/2.1/intro/tutorial01/",
+             "views":40
             },
             {"title":"Django Rocks",
-             "url":"http://djangorocks.com"
+             "url":"http://djangorocks.com",
+             "views":23
             },
             {"title":"How to Tango with Django",
-             "url":"http://www.tangowithdjango.com/"}
+             "url":"http://www.tangowithdjango.com/",
+             "views":25}
+
     ]
 
     other_pages = [
             {"title":"Bottle",
-             "url":"http://bottlepy.org/docs/dev/"
+             "url":"http://bottlepy.org/docs/dev/",
+             "views":20
             },
             {"title":"Flask",
-             "url":"http://flask.pocoo.org"
-            }
+             "url":"http://flask.pocoo.org",
+             "views":10}
     ]
 
     categories = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
@@ -63,7 +71,7 @@ def populate():
         # returns 0 if there is no likes or views field specified in the category dictionary
         c = add_cat(category, likes=category_data.get("likes",0), views=category_data.get("views",0))
         for p in category_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p.get("views", 0)) # same trick here for views
 
     # now we're going to make sure they've actually been added
     # i would personally print show this info differently but again i dont want to risk failing a test due to that
--- rango/views.py
+++ rango/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 
 # views go here
 def index(request):
     # query the db for all categories then return the top 5 liked ones
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     # now pass that to something the template can use
     context_dict = {
       "boldmessage": "Crunchy, creamy, cookie, candy, cupcake!",
       "categories": category_list,
+      "pages": page_list,
     }
     return render(request, 'rango/index.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict["pages"] = pages
+        context_dict["category"] = category
+    except Category.DoesNotExist:
+        context_dict["category"] = None
+        context_dict["pages"] = None
+
+    return render(request, 'rango/category.html', context=context_dict) 
 
 def about(request):
     context_dict = {
