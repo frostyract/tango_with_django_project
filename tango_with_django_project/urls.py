24ceb43b3fcf8d070de4ada252215fc29bbe53fa
frostyract <frostyerroroof@protonmail.com>
2026-02-05 14:22:34 +0000

    chapter 4 with exercises done

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     # handle any rango/ urls in rango
     path('rango/', include('rango.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
